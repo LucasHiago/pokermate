13dd5f4bedbe455a53672411e66b790077cf9fe5
jayhbb <792599135@qq.com>
2017-09-17 16:38:14 +0800

    update
    update

--- apps/home/controllers/LianmengController.php
+++ apps/home/controllers/LianmengController.php
@@ -26,7 +26,7 @@ class LianmengController extends Controller{
 			$id = $mUser->getDefaultLianmengId();
 		}
 		$aLianmengHostDuizhang = $mUser->getLianmengHostDuizhang($id);
-		
+		Yii::info('$aLianmengHostDuizhang:'.json_encode($aLianmengHostDuizhang));
 		return $this->render('lianmeng_host_duizhang', [
 			'aLianmengList' => $aLianmengList,
 			'aLianmengHostDuizhang' => $aLianmengHostDuizhang,
