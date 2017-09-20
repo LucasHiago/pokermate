fb2a1fd91b6d0d5f28343aeef5927760e92a1c21
jayhbb <792599135@qq.com>
2017-09-20 11:11:53 +0800

    update
    update

--- apps/home/controllers/ImportController.php
+++ apps/home/controllers/ImportController.php
@@ -54,7 +54,7 @@ class ImportController extends Controller{
 		
 		$mUser = Yii::$app->user->getIdentity();
 		try{
-			$aDataList = Yii::$app->excel->getSheetDataInArray($fileName);
+			$aDataList = Yii::$app->excel->getSheetDataInArray($fileName);debug($aDataList,11);
 			if($aDataList){
 				foreach($aDataList as $aData){
 					$playerName = (string)$aData[0];
