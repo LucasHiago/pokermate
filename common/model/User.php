fc25f5febe73c1b0a119348fd96bc0b36d0b3d4f
jayhbb <792599135@qq.com>
2017-09-22 01:57:45 +0800

    update
    update

--- common/model/User.php
+++ common/model/User.php
@@ -631,20 +631,29 @@ class User extends \common\lib\DbOrmModel implements IdentityInterface{
 					'lianmeng_shang_zhuo_ren_shu' => 0,
 					'lianmeng_qian_zhang' => $aLianmeng['qianzhang'],
 					'lianmeng_zhang_dan' => 0,
+					'paiju_fee' => $aLianmeng['paiju_fee'],
+					'baoxian_choucheng' => $aLianmeng['baoxian_choucheng'],
+					'duizhangfangfa' => $aLianmeng['duizhangfangfa'],
 				];
 			}
 		}
 		foreach($aResult as $value){
 			//$aReturnList[$value['lianmeng_id']]['lianmeng_zhong_zhang'] += 1;
 			$aReturnList[$value['lianmeng_id']]['lianmeng_shang_zhuo_ren_shu'] += 1;
-			$baoxianBeichou = Calculate::calculateBaoxianBeichou($value['baoxian_heji'], $value['baoxian_choucheng'], $this->choushui_shuanfa);
+			$aReturnList[$value['lianmeng_id']]['zhanji'] += $value['zhanji'];
+			$aReturnList[$value['lianmeng_id']]['baoxian_heji'] += $value['baoxian_heji'];
+			/*$baoxianBeichou = Calculate::calculateBaoxianBeichou($value['baoxian_heji'], $value['baoxian_choucheng'], $this->choushui_shuanfa);
 			if(!$value['is_clean']){
 				$aReturnList[$value['lianmeng_id']]['lianmeng_zhang_dan'] += Calculate::calculateZhangDan($value['zhanji'], $value['baoxian_heji'], $value['paiju_fee'], $baoxianBeichou, $value['duizhangfangfa'], $this->choushui_shuanfa);
-			}
+			}*/
 		}
 		
 		foreach($aReturnList as $lianmengId => $aValue){
 			$aReturnList[$lianmengId]['lianmeng_zhong_zhang'] = $aValue['lianmeng_qian_zhang'] + $aValue['lianmeng_zhang_dan'];
+			$baoxianBeichou = Calculate::calculateBaoxianBeichou($value['baoxian_heji'], $value['baoxian_choucheng'], $this->choushui_shuanfa);
+			if(!$value['is_clean']){
+				$aReturnList[$lianmengId]['lianmeng_zhang_dan'] += Calculate::calculateZhangDan($value['zhanji'], $value['baoxian_heji'], $value['paiju_fee'], $baoxianBeichou, $value['duizhangfangfa'], $this->choushui_shuanfa);
+			}
 		}
 		
 		return $aReturnList;
