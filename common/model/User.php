b8dd96bf526070c9f459996955882845f64f0912
jayhbb <792599135@qq.com>
2017-09-30 23:43:12 +0800

    update
    update

--- common/model/User.php
+++ common/model/User.php
@@ -576,7 +576,8 @@ class User extends \common\lib\DbOrmModel implements IdentityInterface{
 			$aReturnList[$paijuId]['lianmeng_butie'] = $lianmengButie;
 			$aReturnList[$paijuId]['float_lianmeng_butie'] = $floatLianmengButie;
 			$aReturnList[$paijuId]['shiji_choushui_value'] = Calculate::calculateShijiChouShuiValue($v['choushui_value'], $lianmengButie, $v['paiju_fee'], $this->choushui_shuanfa);
-			$aReturnList[$paijuId]['float_shiji_choushui_value'] = Calculate::calculateShijiChouShuiValue($v['float_choushui_value'], $floatLianmengButie, $v['paiju_fee'], $this->choushui_shuanfa, false);
+			//$aReturnList[$paijuId]['float_shiji_choushui_value'] = Calculate::calculateShijiChouShuiValue($v['float_choushui_value'], $floatLianmengButie, $v['paiju_fee'], $this->choushui_shuanfa, false);
+			$aReturnList[$paijuId]['float_shiji_choushui_value'] = Calculate::calculateShijiChouShuiValue($v['choushui_value'], $floatLianmengButie, $v['paiju_fee'], $this->choushui_shuanfa, false);
 		}
 		foreach($aReturnList as $paijuId => $v){
 			$aReturnList[$paijuId]['int_float_shiji_choushui_value'] = Calculate::getIntValueByChoushuiShuanfa($aReturnList[$paijuId]['float_shiji_choushui_value'], $this->choushui_shuanfa);
