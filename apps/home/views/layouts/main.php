e328db75b9375d10450a8f6365bd879e8bcf029c
jayhbb <792599135@qq.com>
2017-10-22 03:35:32 +0800

    update
    update

--- apps/home/views/layouts/main.php
+++ apps/home/views/layouts/main.php
@@ -220,7 +220,7 @@ if($mUser){
 							<ul class="nav navbar-nav">
 								<li class="J-c-h-t-menu-m1"><a href="<?php echo Url::to('home', 'index/index'); ?>" style="width:138px;text-align:center;font-weight:bold;font-size:18px;">结账台</a></li>
 								<li class="J-c-h-t-menu-m2"><a href="<?php echo Url::to('home', 'agent/index'); ?>" style="width:138px;text-align:center;font-weight:bold;font-size:18px;">代理分成</a></li>
-								<li class="J-c-h-t-menu-m3"><a href="<?php echo Url::to('home', 'lianmeng/lianmeng-host-duizhang'); ?>" style="width:138p;text-align:center;font-weight:bold;font-size:18px;">联盟主机对账</a></li>
+								<li class="J-c-h-t-menu-m3"><a href="<?php echo Url::to('home', 'lianmeng/lianmeng-host-duizhang'); ?>" style="width:138px;text-align:center;font-weight:bold;font-size:18px;">联盟主机对账</a></li>
 							</ul>
 							<a href="javascript:;" class="btn btn-lg btn-primary" style="float:right;position:relative;right:16px;top:3px;" onclick="AlertWin.showJiaoBanZhuanChu();">交班账单</a>
 						</div>
