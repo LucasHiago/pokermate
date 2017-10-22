ec4a4216ff21c373f07d57d7e8e50c6eba2adcf4
jayhbb <792599135@qq.com>
2017-10-23 01:38:57 +0800

    update
    update

--- resource/plug-in/js.paiju.list.js
+++ resource/plug-in/js.paiju.list.js
@@ -70,7 +70,7 @@
 		function _appendList(aData){
 			var html = '';
 			for(var i in aData){
-				html += '<div class="panel panel-' + (aData[i].status == 0 ? 'yellow' : 'green') + ' paiju-item">';
+				html += '<div class="panel panel-' + (aData[i].status == 0 ? 'yellow' : 'green') + ' paiju-item" style="margin-left: 13px;">';
 					html += '<div class="panel-heading">';
 						html += '<h3 class="panel-title" onclick="AlertWin.showPaijuDataList(' + aData[i].id + ', 1);">' + aData[i].paiju_name + '</h3>';
 					html += '</div>';
