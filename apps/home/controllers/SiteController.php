157dd936137a74f652ebbf0fa5bdd270f526a3ba
jayhbb <792599135@qq.com>
2017-11-18 18:46:42 +0800

    update
    update

--- apps/home/controllers/SiteController.php
+++ apps/home/controllers/SiteController.php
@@ -12,8 +12,40 @@ class SiteController extends Controller{
 		 $this->layout = 'login'; 
 		return $this->render('index');
 	}
+	
+	private function _ajustFengChengSettingSort(){
+		set_time_limit(0);
+		$aUserList = \common\model\User::findAll();
+		$aFenchengSetting = \common\model\FenchengSetting::getFenchengConfigList();
+		foreach($aUserList as $aUser){
+			$mUser = \common\model\User::toModel($aUser);
+			$aAgentList = $mUser->getAgentList();
+			foreach($aAgentList as $aAgent){
+				$aFcsting = $mUser->getFenchengListSetting($aAgent['id']);
+				$aFcstingCp = $aFcsting;
+				
+				foreach($aFenchengSetting as $i => $aFencheng){
+					$mFenchengSetting = \common\model\FenchengSetting::toModel($aFcsting[$i]);
+					$mFenchengSetting->set('zhuozi_jibie', $aFencheng);
+					foreach($aFcstingCp as $acp){
+						if($acp['zhuozi_jibie'] == $aFencheng){
+							$mFenchengSetting->set('yingfan', $acp['yingfan']);
+							$mFenchengSetting->set('shufan', $acp['shufan']);
+							break;
+						}
+					}
+					$mFenchengSetting->save();
+				}
+			}
+			
+		}
+		
+		debug('complete');
+	}
 
 	public function actionTest(){
+		$this->_ajustFengChengSettingSort();
+		
 		/*$sql = "SELECT * from lianmeng where lmzj_paiju_creater!=''";
 		$aResult = Yii::$app->db->createCommand($sql)->queryAll();
 		debug($aResult);
